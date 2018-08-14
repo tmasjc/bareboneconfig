e89142628af141b7812905c63856e05443758895
Thomas Jc <thomas.jcng@gmail.com>
2018-08-14 11:07:28 +0800

    Improve code structure.

--- main.R
+++ main.R
@@ -3,82 +3,143 @@ library(plumber)
 library(purrr)
 library(magrittr)
 
-# Main Function Goes Here -------------------------------------------------
+
 
 # default path to file
-DEFAULT_PATH = "~/rconfig.yaml"
+get_default_path <- function() {
+    
+    # Not tested on Windows
+    return("~/rconfig.yaml")
+}
+
+
+# Vault Operation --------------------------------------------------------------
+
+
+open_vault <- function(secret, path = NULL) {
+    
+    #browser()
+    
+    # assign default path
+    if(is.null(path)) {
+        path <- get_default_path()
+    }
+    
+    # return 0 for success, 8 for failure
+    status <- tryCatch(
+        system(sprintf("ccrypt -d %s -K %s", paste0(path, ".cpt"), secret)),
+        error = function(e) e
+    )
+    
+    if(status != 0) {
+        stop("Vault cannot be opened.")
+    }
+    
+    read_yaml(path)
+    
+}
+
+close_vault <- function(secret, configs = NULL, path = NULL) {
+    
+    #browser()
+    
+    # assign default path
+    if(is.null(path)) {
+        path <- get_default_path()
+    }
+    
+    if(!is.null(configs)) {
+        # update file
+        write(as.yaml(configs), file = path)
+    }
+    
+    # return 0 for success, 8 for failure
+    status <- tryCatch(
+        system(sprintf("ccrypt %s -K %s", path, secret)),
+        error = function(e) e
+    )
+    
+    if(status != 0) {
+        stop("Vault cannot be closed.")
+    }
+    
+}
+ 
+
+# Get  --------------------------------------------------
 
 # decrypt, read, encrypt
-get_config <- function(secret, which, path = DEFAULT_PATH) {
+get_config <- function(secret, which) {
     
-    system(sprintf("ccrypt -d %s -K %s", paste0(path, ".cpt"), secret))
-    
-    configs <- read_yaml(path)
+    configs <- open_vault(secret)
     
     # get key
     kv <- configs[[which]]
     
-    system(sprintf("ccrypt %s -K %s", path, secret))
+    close_vault(secret)
     
     # return
     kv
     
 }
 
+
+# Update ------------------------------------------------------------------
+
+
 # decrypt, read, update, encrypt
-mod_config <- function(secret, which, key, value, path = DEFAULT_PATH) {
+mod_config <- function(secret, which, key, value) {
     
-    system(sprintf("ccrypt -d %s -K %s", paste0(path, ".cpt"), secret))
+    configs <- open_vault(secret)
     
-    configs <- read_yaml(path)
-    
+    # update key
     configs[[which]][key] <- value
     
-    # update file
-    write(as.yaml(configs), file = DEFAULT_PATH)
-    
-    system(sprintf("ccrypt %s -K %s", path, secret))
+    close_vault(secret, configs)
     
     "Success"
     
 }
+
+
+# Delete ------------------------------------------------------------------
+
 
 # decrypt, read, delete, encrypt
-rm_key <- function(secret, which, key, path = DEFAULT_PATH) {
+drop_key <- function(secret, which, key) {
     
-    system(sprintf("ccrypt -d %s -K %s", paste0(path, ".cpt"), secret))
+    configs <- open_vault(secret)
     
-    configs <- read_yaml(path)
-    
+    # drop key
     configs[[which]][key] <- NULL
     
-    # update file
-    write(as.yaml(configs), file = DEFAULT_PATH)
-    
-    system(sprintf("ccrypt %s -K %s", path, secret))
+    close_vault(secret, configs)
     
     "Success"
     
 }
 
+
+# Insert ------------------------------------------------------------------
+
+
 # decrypt, read, insert, encrypt
-insert_config <- function(secret, which, kvlist, path = DEFAULT_PATH) {
+insert_config <- function(secret, which, kvlist) {
     
-    system(sprintf("ccrypt -d %s -K %s", paste0(path, ".cpt"), secret))
-    
-    configs <- read_yaml(path)
+    configs <- open_vault(secret)
     
     # insert new config
     configs[[which]] <- kvlist
     
-    # update file
-    write(as.yaml(configs), file = DEFAULT_PATH)
-    
-    system(sprintf("ccrypt %s -K %s", path, secret))
+    close_vault(secret, configs)
     
     "Success"
     
 }
+
+
+# Plumber -----------------------------------------------------------------
+
 
 #* @get /config
 function(token, id) {
@@ -92,11 +153,13 @@ function(token, id, key, value) {
 
 #* @get /delete
 function(token, id, key) {
-    rm_key(token, id, key)
+    drop_key(token, id, key)
 }
 
 #* @post /insert
 function(req, token, id) {
+    
+    #browser()
     
     # parse request body
     zz <- strsplit(req$postBody, split = "&") %>% 
--- plumber.R
+++ plumber.R
@@ -1,3 +1,4 @@
 library(plumber)
 r <- plumb("main.R")
 r$run(host = "0.0.0.0", port = 7788)
+
